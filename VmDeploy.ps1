c7954448eea5d92702da0c987ce382dacb0a7751
Mehodge <41706944+mehodge@users.noreply.github.com>
2021-10-05 21:10:45 +0100

    Update VmDeploy.ps1
    removed secrets

--- VmDeploy.ps1
+++ VmDeploy.ps1
@@ -1,10 +1,10 @@
-$TenantId = '5c147d8a-3229-4a42-a54e-98b937904e15'
-$SubID = 'bad8333d-60e3-47ec-884d-20456f2df201'
+$TenantId = ''
+$SubID = ''
 $RG = 'cloudTeamGitProject'
 $Name = 'Server2019 Deployment'
 $Location = 'North Europe'
-$TemplateFile = "C:\Users\MehodgeAdmin\OneDrive\Git\cloudTeamGitProject\azuredeploy.json"
-$TemplateParameterFile = "C:\Users\MehodgeAdmin\OneDrive\Git\cloudTeamGitProject\azuredeploy.parameters.json"
+$TemplateFile = "azuredeploy.json"
+$TemplateParameterFile = "azuredeploy.parameters.json"
 
 Connect-AzAccount -TenantId $TenantId -SubscriptionID $SubID
 
