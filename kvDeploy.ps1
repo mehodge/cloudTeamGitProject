a02fb4defadb072c673f1bd9fff8fcfe178588e8
Mehodge <41706944+mehodge@users.noreply.github.com>
2021-10-05 21:11:50 +0100

    Update kvDeploy.ps1
    removed secrets

--- kvDeploy.ps1
+++ kvDeploy.ps1
@@ -1,5 +1,5 @@
-$TenantId = '5c147d8a-3229-4a42-a54e-98b937904e15'
-$SubID = 'bad8333d-60e3-47ec-884d-20456f2df201'
+$TenantId = ''
+$SubID = ''
 $RG = "Shared-RG"
 $Location = "North Europe"
 $VaultName = "LocalAdmin-KV"
@@ -22,7 +22,7 @@ New-AzKeyVault `
 	Vendor="Mehodge"	  
   }
 
-$secretvalue = ConvertTo-SecureString 'y6f_j($/r+]>-e7AJ+;:2bM#Z' -AsPlainText -Force
+$secretvalue = ConvertTo-SecureString 'a-password' -AsPlainText -Force
 $secret = Set-AzKeyVaultSecret -VaultName $VaultName -Name 'azadmin' -SecretValue $secretvalue `
 	-Tag @{
 		Application="Identity";
